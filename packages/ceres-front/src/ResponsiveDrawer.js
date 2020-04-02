2543ce4e1d1e72aea56719b58726a963c40e43c3
leymann <loriane.eymann@gmail.com>
2020-04-02 19:30:29 +0200

    feat: Replace fixed drawer with responsive drawer

---
+++ packages/ceres-front/src/ResponsiveDrawer.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import {ThemeProvider} from '@material-ui/core/styles'
+import {useStyles, theme} from './style'
+import DrawerContent from "./DrawerContent";
+
+function ResponsiveDrawer() {
+  const classes = useStyles();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <CssBaseline/>
+        <nav className={classes.drawer} aria-label="app folders">
+          {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+          <Hidden smUp implementation="css">
+            <Drawer
+              variant="temporary"
+              anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+              open={mobileOpen}
+              onClose={handleDrawerToggle}
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+              ModalProps={{
+                keepMounted: true, // Better open performance on mobile.
+              }}
+            >
+              <DrawerContent />
+            </Drawer>
+          </Hidden>
+          <Hidden xsDown implementation="css">
+            <Drawer
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+              variant="permanent"
+              open
+            >
+              <DrawerContent />
+            </Drawer>
+          </Hidden>
+        </nav>
+        <main className={classes.content}>
+          <AppBar position="fixed" className={classes.appBar}>
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="start"
+                onClick={handleDrawerToggle}
+                className={classes.menuButton}
+              >
+                <MenuIcon/>
+              </IconButton>
+              <Typography variant="h6" noWrap style={{color: "white"}}>
+                Liste de courses
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <div className={classes.toolbar}/>
+          <Typography paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+            facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+            gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+            donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+            adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+            Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+            imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+            arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+            donec massa sapien faucibus et molestie ac.
+          </Typography>
+        </main>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default ResponsiveDrawer;
--- packages/ceres-front/src/style.js
+++ packages/ceres-front/src/style.js
@@ -5,34 +5,52 @@ import lightGreen from '@material-ui/core/colors/lightGreen'
 const drawerWidth = 240;
 
 export const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  appBar: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  // necessary for content to be below app bar
-  toolbar: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
-    padding: theme.spacing(3),
-  },
-  fitPicture: {
-    'margin-left': 'auto',
-    'margin-right': 'auto',
-    'margin-top': 20,
-    'margin-bottom': 20
-  }
-}));
+	root: {
+		display: 'flex',
+	},
+	drawer: {
+		[theme.breakpoints.up('sm')]: {
+			width: drawerWidth,
+			flexShrink: 0,
+		},
+	},
+	appBar: {
+		[theme.breakpoints.up('sm')]: {
+			width: `calc(100% - ${drawerWidth}px)`,
+			marginLeft: drawerWidth,
+		},
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+		[theme.breakpoints.up('sm')]: {
+			display: 'none',
+		},
+	},
+	// necessary for content to be below app bar
+	toolbar: theme.mixins.toolbar,
+	drawerPaper: {
+		width: drawerWidth,
+	},
+	content: {
+		flexGrow: 1,
+		padding: theme.spacing(3),
+	},
+	fitPicture: {
+		'padding-left': 50,
+	  'padding-right': 50,
+	  'margin-bottom': 10,
+	  'margin-top': 10,
+		width: '100%',
+		[theme.breakpoints.up('sm')]: {
+      'margin-left': 'auto',
+      'margin-right': 'auto',
+      'margin-top': 20,
+      'margin-bottom': 20,
+      'padding-left': 0,
+      'padding-right': 0,
+    },
+	}
+ }));
 
 export const theme = createMuiTheme({
   palette: {
