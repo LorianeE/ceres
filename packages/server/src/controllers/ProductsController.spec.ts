5c67b6219acf3266adc2576c4df69da9f44ca45b
leymann <loriane.eymann@gmail.com>
2020-04-20 20:47:22 +0200

    chore: prettier and package.json test script for clientt

--- packages/server/src/controllers/ProductsController.spec.ts
+++ packages/server/src/controllers/ProductsController.spec.ts
@@ -7,126 +7,134 @@ import {DuplicateKeyError} from "../services/errors/DuplicateKeyError";
 import {BadRequest} from "ts-httpexceptions";
 
 describe("ProductsController", () => {
-    describe("get()", () => {
-        beforeEach(() => TestContext.create());
-        afterEach(() => TestContext.reset());
+  describe("get()", () => {
+    beforeEach(() => TestContext.create());
+    afterEach(() => TestContext.reset());
 
-        it("should return a result from mocked service", async () => {
-            // GIVEN
-            const productsService = {
-                findAll: jest.fn().mockResolvedValue([{id: "eggs"}])
-            };
+    it("should return a result from mocked service", async () => {
+      // GIVEN
+      const productsService = {
+        findAll: jest.fn().mockResolvedValue([{id: "eggs"}]),
+      };
 
-            const productsCtrl = await TestContext.invoke(ProductsController, [{
-                provide: ProductsService,
-                use: productsService
-            }]);
+      const productsCtrl = await TestContext.invoke(ProductsController, [
+        {
+          provide: ProductsService,
+          use: productsService,
+        },
+      ]);
 
-            // WHEN
-            const result = await productsCtrl.get();
+      // WHEN
+      const result = await productsCtrl.get();
 
-            // THEN
-            expect(result).toEqual([{id: "eggs"}]);
-            expect(productsService.findAll).toHaveBeenCalled();
+      // THEN
+      expect(result).toEqual([{id: "eggs"}]);
+      expect(productsService.findAll).toHaveBeenCalled();
 
-            expect(productsCtrl).toBeInstanceOf(ProductsController);
-            expect(productsCtrl.productsService).toEqual(productsService);
-        });
+      expect(productsCtrl).toBeInstanceOf(ProductsController);
+      expect(productsCtrl.productsService).toEqual(productsService);
     });
-    describe("addProducts()", () => {
-        const product = new Product();
-        product.id = "apple";
-        product.label = "Pommes";
-        product.shelf = ShelfTypes.PRODUCE;
+  });
+  describe("addProducts()", () => {
+    const product = new Product();
+    product.id = "apple";
+    product.label = "Pommes";
+    product.shelf = ShelfTypes.PRODUCE;
 
-        const products = [product];
+    const products = [product];
 
-        describe("when everything is ok", () => {
-            beforeEach(() => TestContext.create());
-            afterEach(() => TestContext.reset());
+    describe("when everything is ok", () => {
+      beforeEach(() => TestContext.create());
+      afterEach(() => TestContext.reset());
 
-            it("should return saved data", async () => {
-                // GIVEN
-                const productsService = {
-                    save: jest.fn().mockResolvedValue(product)
-                };
+      it("should return saved data", async () => {
+        // GIVEN
+        const productsService = {
+          save: jest.fn().mockResolvedValue(product),
+        };
 
-                const productsCtrl = await TestContext.invoke(ProductsController, [{
-                    provide: ProductsService,
-                    use: productsService
-                }]);
+        const productsCtrl = await TestContext.invoke(ProductsController, [
+          {
+            provide: ProductsService,
+            use: productsService,
+          },
+        ]);
 
-                // WHEN
-                const result = await productsCtrl.addProducts(products);
+        // WHEN
+        const result = await productsCtrl.addProducts(products);
 
-                // THEN
-                expect(productsService.save).toHaveBeenCalledTimes(1);
-                expect(productsService.save).toHaveBeenCalledWith(product);
-            });
-        });
-        describe("when there is a problem with duplicate key", () => {
-            beforeEach(() => TestContext.create());
-            afterEach(() => TestContext.reset());
-
-            it("should throw BadRequest error when DuplicateKeyError", async () => {
-                // GIVEN
-                const error = new DuplicateKeyError();
-                error.message = "One key is already in DB";
-
-                const productsService = {
-                    save: jest.fn().mockRejectedValue(error)
-                };
-
-                const productsCtrl = await TestContext.invoke(ProductsController, [{
-                    provide: ProductsService,
-                    use: productsService
-                }]);
-
-                // WHEN
-                let actualError;
-                try {
-                    await productsCtrl.addProducts(products);
-                } catch (er) {
-                    actualError = er;
-                }
-                // THEN
-
-                expect(productsCtrl.productsService).toEqual(productsService);
-                expect(productsService.save).toHaveBeenCalledTimes(1);
-                expect(productsService.save).toHaveBeenCalledWith(product);
-                expect(actualError).toBeInstanceOf(BadRequest);
-                expect(actualError.message).toEqual("One key is already in DB");
-            });
-        });
-        describe("when there is an other problem", () => {
-            beforeEach(() => TestContext.create());
-            afterEach(() => TestContext.reset());
-
-            it("should throw BadRequest error when DuplicateKeyError", async () => {
-                // GIVEN
-                const productsService = {
-                    save: jest.fn().mockRejectedValue(new Error())
-                };
-
-                const productsCtrl = await TestContext.invoke(ProductsController, [{
-                    provide: ProductsService,
-                    use: productsService
-                }]);
-
-                // WHEN
-                let actualError;
-                try {
-                    await productsCtrl.addProducts(products);
-                } catch (er) {
-                    actualError = er;
-                }
-                // THEN
-
-                expect(productsCtrl.productsService).toEqual(productsService);
-                expect(productsService.save).toHaveBeenCalledTimes(1);
-                expect(productsService.save).toHaveBeenCalledWith(product);
-                expect(actualError).toBeInstanceOf(Error);
-            });
-        });
+        // THEN
+        expect(productsService.save).toHaveBeenCalledTimes(1);
+        expect(productsService.save).toHaveBeenCalledWith(product);
+      });
     });
+    describe("when there is a problem with duplicate key", () => {
+      beforeEach(() => TestContext.create());
+      afterEach(() => TestContext.reset());
+
+      it("should throw BadRequest error when DuplicateKeyError", async () => {
+        // GIVEN
+        const error = new DuplicateKeyError();
+        error.message = "One key is already in DB";
+
+        const productsService = {
+          save: jest.fn().mockRejectedValue(error),
+        };
+
+        const productsCtrl = await TestContext.invoke(ProductsController, [
+          {
+            provide: ProductsService,
+            use: productsService,
+          },
+        ]);
+
+        // WHEN
+        let actualError;
+        try {
+          await productsCtrl.addProducts(products);
+        } catch (er) {
+          actualError = er;
+        }
+        // THEN
+
+        expect(productsCtrl.productsService).toEqual(productsService);
+        expect(productsService.save).toHaveBeenCalledTimes(1);
+        expect(productsService.save).toHaveBeenCalledWith(product);
+        expect(actualError).toBeInstanceOf(BadRequest);
+        expect(actualError.message).toEqual("One key is already in DB");
+      });
+    });
+    describe("when there is an other problem", () => {
+      beforeEach(() => TestContext.create());
+      afterEach(() => TestContext.reset());
+
+      it("should throw BadRequest error when DuplicateKeyError", async () => {
+        // GIVEN
+        const productsService = {
+          save: jest.fn().mockRejectedValue(new Error()),
+        };
+
+        const productsCtrl = await TestContext.invoke(ProductsController, [
+          {
+            provide: ProductsService,
+            use: productsService,
+          },
+        ]);
+
+        // WHEN
+        let actualError;
+        try {
+          await productsCtrl.addProducts(products);
+        } catch (er) {
+          actualError = er;
+        }
+        // THEN
+
+        expect(productsCtrl.productsService).toEqual(productsService);
+        expect(productsService.save).toHaveBeenCalledTimes(1);
+        expect(productsService.save).toHaveBeenCalledWith(product);
+        expect(actualError).toBeInstanceOf(Error);
+      });
+    });
+  });
 });
--- packages/server/src/controllers/ShoppingListsController.spec.ts
+++ packages/server/src/controllers/ShoppingListsController.spec.ts
@@ -5,195 +5,208 @@ import {BadRequest, NotFound} from "ts-httpexceptions";
 import {ShoppingList} from "../models/ShoppingList";
 
 describe("ShoppingListsController", () => {
-    describe("get()", () => {
-        beforeEach(() => TestContext.create());
-        afterEach(() => TestContext.reset());
+  describe("get()", () => {
+    beforeEach(() => TestContext.create());
+    afterEach(() => TestContext.reset());
 
-        it("should return a result from shoppinglistservice if not undefined", async () => {
-            // GIVEN
-            const shoppingListService = {
-                find: jest.fn().mockResolvedValue({id: "1", items: []})
-            };
+    it("should return a result from shoppinglistservice if not undefined", async () => {
+      // GIVEN
+      const shoppingListService = {
+        find: jest.fn().mockResolvedValue({id: "1", items: []}),
+      };
 
-            const shoppingListCtrl = await TestContext.invoke(ShoppingListsController, [{
-                provide: ShoppingListService,
-                use: shoppingListService
-            }]);
+      const shoppingListCtrl = await TestContext.invoke(ShoppingListsController, [
+        {
+          provide: ShoppingListService,
+          use: shoppingListService,
+        },
+      ]);
 
-            // WHEN
-            const result = await shoppingListCtrl.get({}, "1");
+      // WHEN
+      const result = await shoppingListCtrl.get({}, "1");
 
-            // THEN
-            expect(result).toEqual({id: "1", items: []});
-            expect(shoppingListService.find).toHaveBeenCalledTimes(1);
-            expect(shoppingListService.find).toHaveBeenCalledWith("1");
+      // THEN
+      expect(result).toEqual({id: "1", items: []});
+      expect(shoppingListService.find).toHaveBeenCalledTimes(1);
+      expect(shoppingListService.find).toHaveBeenCalledWith("1");
 
-            expect(shoppingListCtrl).toBeInstanceOf(ShoppingListsController);
-            expect(shoppingListCtrl.shoppingListService).toEqual(shoppingListService);
-        });
-
-        it("should return notfound error if result from shoppinglistservice is undefined", async () => {
-            // GIVEN
-            const shoppingListService = {
-                find: jest.fn().mockResolvedValue(undefined)
-            };
-
-            const shoppingListCtrl = await TestContext.invoke(ShoppingListsController, [{
-                provide: ShoppingListService,
-                use: shoppingListService
-            }]);
-
-            // WHEN
-            let actualError;
-            try {
-                const result = await shoppingListCtrl.get({}, "1");
-            } catch (e) {
-                actualError = e;
-            }
-
-            // THEN
-            expect(shoppingListCtrl.shoppingListService).toEqual(shoppingListService);
-            expect(shoppingListService.find).toHaveBeenCalledTimes(1);
-            expect(shoppingListService.find).toHaveBeenCalledWith("1");
-            expect(actualError).toBeInstanceOf(NotFound);
-        });
+      expect(shoppingListCtrl).toBeInstanceOf(ShoppingListsController);
+      expect(shoppingListCtrl.shoppingListService).toEqual(shoppingListService);
     });
-    describe("create()", () => {
-        beforeEach(() => TestContext.create());
-        afterEach(() => TestContext.reset());
 
-        it("should return a result from shoppinglistservice if not undefined", async () => {
-            // GIVEN
-            const shoppingList = new ShoppingList();
-            shoppingList._id = "1234";
-            shoppingList.items = [];
+    it("should return notfound error if result from shoppinglistservice is undefined", async () => {
+      // GIVEN
+      const shoppingListService = {
+        find: jest.fn().mockResolvedValue(undefined),
+      };
 
-            const shoppingListService = {
-                save: jest.fn().mockResolvedValue(shoppingList)
-            };
+      const shoppingListCtrl = await TestContext.invoke(ShoppingListsController, [
+        {
+          provide: ShoppingListService,
+          use: shoppingListService,
+        },
+      ]);
 
-            const shoppingListCtrl = await TestContext.invoke(ShoppingListsController, [{
-                provide: ShoppingListService,
-                use: shoppingListService
-            }]);
+      // WHEN
+      let actualError;
+      try {
+        const result = await shoppingListCtrl.get({}, "1");
+      } catch (e) {
+        actualError = e;
+      }
 
-            // WHEN
-            const result = await shoppingListCtrl.create(shoppingList);
-
-            // THEN
-            expect(shoppingListService.save).toHaveBeenCalledTimes(1);
-            expect(shoppingListService.save).toHaveBeenCalledWith(shoppingList);
-            expect(result).toEqual(shoppingList);
-        });
-        it("should throw error if shoppinglistservice throws error", async () => {
-            // GIVEN
-            const shoppingList = new ShoppingList();
-            shoppingList._id = "1234";
-            shoppingList.items = [];
-
-            const shoppingListService = {
-                save: jest.fn().mockRejectedValue(new Error("An error occured"))
-            };
-
-            const shoppingListCtrl = await TestContext.invoke(ShoppingListsController, [{
-                provide: ShoppingListService,
-                use: shoppingListService
-            }]);
-
-            // WHEN
-            let actualError;
-            try {
-                await shoppingListCtrl.create(shoppingList);
-            } catch (e) {
-                actualError = e;
-            }
-
-            // THEN
-            expect(shoppingListService.save).toHaveBeenCalledTimes(1);
-            expect(actualError).toBeInstanceOf(Error);
-        });
+      // THEN
+      expect(shoppingListCtrl.shoppingListService).toEqual(shoppingListService);
+      expect(shoppingListService.find).toHaveBeenCalledTimes(1);
+      expect(shoppingListService.find).toHaveBeenCalledWith("1");
+      expect(actualError).toBeInstanceOf(NotFound);
     });
-    describe("update()", () => {
-        beforeEach(() => TestContext.create());
-        afterEach(() => TestContext.reset());
+  });
+  describe("create()", () => {
+    beforeEach(() => TestContext.create());
+    afterEach(() => TestContext.reset());
 
-        it("should return a result from shoppinglistservice if ok", async () => {
-            // GIVEN
-            const shoppingList = new ShoppingList();
-            shoppingList._id = "1234";
-            shoppingList.items = [];
+    it("should return a result from shoppinglistservice if not undefined", async () => {
+      // GIVEN
+      const shoppingList = new ShoppingList();
+      shoppingList._id = "1234";
+      shoppingList.items = [];
 
-            const shoppingListService = {
-                save: jest.fn().mockResolvedValue(shoppingList)
-            };
+      const shoppingListService = {
+        save: jest.fn().mockResolvedValue(shoppingList),
+      };
 
-            const shoppingListCtrl = await TestContext.invoke(ShoppingListsController, [{
-                provide: ShoppingListService,
-                use: shoppingListService
-            }]);
+      const shoppingListCtrl = await TestContext.invoke(ShoppingListsController, [
+        {
+          provide: ShoppingListService,
+          use: shoppingListService,
+        },
+      ]);
 
-            // WHEN
-            const result = await shoppingListCtrl.update("1234", shoppingList);
+      // WHEN
+      const result = await shoppingListCtrl.create(shoppingList);
 
-            // THEN
-            expect(shoppingListService.save).toHaveBeenCalledTimes(1);
-            expect(shoppingListService.save).toHaveBeenCalledWith(shoppingList);
-            expect(result).toEqual(shoppingList);
-        });
-        it("should throw BadRequest if shoppinglist id does not match with path id", async () => {
-            // GIVEN
-            const shoppingList = new ShoppingList();
-            shoppingList._id = "1";
-            shoppingList.items = [];
-
-            const shoppingListService = {
-                save: jest.fn().mockResolvedValue(shoppingList)
-            };
-
-            const shoppingListCtrl = await TestContext.invoke(ShoppingListsController, [{
-                provide: ShoppingListService,
-                use: shoppingListService
-            }]);
-
-            // WHEN
-            let actualError;
-            try {
-                const result = await shoppingListCtrl.update("1234", shoppingList);
-            } catch (e) {
-                actualError = e;
-            }
-
-            // THEN
-            expect(shoppingListService.save).toHaveBeenCalledTimes(0);
-            expect(actualError).toBeInstanceOf(BadRequest);
-        });
-        it("should throw error if shoppinglistservice throws error", async () => {
-            // GIVEN
-            const shoppingList = new ShoppingList();
-            shoppingList._id = "1234";
-            shoppingList.items = [];
-
-            const shoppingListService = {
-                save: jest.fn().mockRejectedValue(new Error("An error occured"))
-            };
-
-            const shoppingListCtrl = await TestContext.invoke(ShoppingListsController, [{
-                provide: ShoppingListService,
-                use: shoppingListService
-            }]);
-
-            // WHEN
-            let actualError;
-            try {
-                await shoppingListCtrl.update("1234", shoppingList);
-            } catch (e) {
-                actualError = e;
-            }
-
-            // THEN
-            expect(shoppingListService.save).toHaveBeenCalledTimes(1);
-            expect(actualError).toBeInstanceOf(Error);
-        });
+      // THEN
+      expect(shoppingListService.save).toHaveBeenCalledTimes(1);
+      expect(shoppingListService.save).toHaveBeenCalledWith(shoppingList);
+      expect(result).toEqual(shoppingList);
     });
-})
-;
+    it("should throw error if shoppinglistservice throws error", async () => {
+      // GIVEN
+      const shoppingList = new ShoppingList();
+      shoppingList._id = "1234";
+      shoppingList.items = [];
+
+      const shoppingListService = {
+        save: jest.fn().mockRejectedValue(new Error("An error occured")),
+      };
+
+      const shoppingListCtrl = await TestContext.invoke(ShoppingListsController, [
+        {
+          provide: ShoppingListService,
+          use: shoppingListService,
+        },
+      ]);
+
+      // WHEN
+      let actualError;
+      try {
+        await shoppingListCtrl.create(shoppingList);
+      } catch (e) {
+        actualError = e;
+      }
+
+      // THEN
+      expect(shoppingListService.save).toHaveBeenCalledTimes(1);
+      expect(actualError).toBeInstanceOf(Error);
+    });
+  });
+  describe("update()", () => {
+    beforeEach(() => TestContext.create());
+    afterEach(() => TestContext.reset());
+
+    it("should return a result from shoppinglistservice if ok", async () => {
+      // GIVEN
+      const shoppingList = new ShoppingList();
+      shoppingList._id = "1234";
+      shoppingList.items = [];
+
+      const shoppingListService = {
+        save: jest.fn().mockResolvedValue(shoppingList),
+      };
+
+      const shoppingListCtrl = await TestContext.invoke(ShoppingListsController, [
+        {
+          provide: ShoppingListService,
+          use: shoppingListService,
+        },
+      ]);
+
+      // WHEN
+      const result = await shoppingListCtrl.update("1234", shoppingList);
+
+      // THEN
+      expect(shoppingListService.save).toHaveBeenCalledTimes(1);
+      expect(shoppingListService.save).toHaveBeenCalledWith(shoppingList);
+      expect(result).toEqual(shoppingList);
+    });
+    it("should throw BadRequest if shoppinglist id does not match with path id", async () => {
+      // GIVEN
+      const shoppingList = new ShoppingList();
+      shoppingList._id = "1";
+      shoppingList.items = [];
+
+      const shoppingListService = {
+        save: jest.fn().mockResolvedValue(shoppingList),
+      };
+
+      const shoppingListCtrl = await TestContext.invoke(ShoppingListsController, [
+        {
+          provide: ShoppingListService,
+          use: shoppingListService,
+        },
+      ]);
+
+      // WHEN
+      let actualError;
+      try {
+        const result = await shoppingListCtrl.update("1234", shoppingList);
+      } catch (e) {
+        actualError = e;
+      }
+
+      // THEN
+      expect(shoppingListService.save).toHaveBeenCalledTimes(0);
+      expect(actualError).toBeInstanceOf(BadRequest);
+    });
+    it("should throw error if shoppinglistservice throws error", async () => {
+      // GIVEN
+      const shoppingList = new ShoppingList();
+      shoppingList._id = "1234";
+      shoppingList.items = [];
+
+      const shoppingListService = {
+        save: jest.fn().mockRejectedValue(new Error("An error occured")),
+      };
+
+      const shoppingListCtrl = await TestContext.invoke(ShoppingListsController, [
+        {
+          provide: ShoppingListService,
+          use: shoppingListService,
+        },
+      ]);
+
+      // WHEN
+      let actualError;
+      try {
+        await shoppingListCtrl.update("1234", shoppingList);
+      } catch (e) {
+        actualError = e;
+      }
+
+      // THEN
+      expect(shoppingListService.save).toHaveBeenCalledTimes(1);
+      expect(actualError).toBeInstanceOf(Error);
+    });
+  });
+});
