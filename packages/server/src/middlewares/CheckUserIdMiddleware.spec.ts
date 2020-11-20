e6d9a7eee31c0c10a5d18ed6cf3ab8bde75bb474
leymann <lo.eymann@gmail.com>
2020-11-20 18:23:49 +0100

    chore(server): Add middlewares

---
+++ packages/server/src/middlewares/CheckUserIdMiddleware.spec.ts
@@ -0,0 +1,46 @@
+import {PlatformTest} from "@tsed/common";
+import {CheckUserIdMiddleware} from "./CheckUserIdMiddleware";
+import {Unauthorized} from "@tsed/exceptions";
+import User from "../models/User";
+
+describe("CheckUserIdMiddleware", () => {
+  beforeEach(PlatformTest.create);
+  afterEach(PlatformTest.reset);
+
+  it("should throw Unauthorized error if user id does not match authorized user id", async () => {
+    // GIVEN
+    const checkUserIdMiddleware = PlatformTest.get<CheckUserIdMiddleware>(CheckUserIdMiddleware);
+    const userId = "userId";
+    const user = new User();
+    user._id = "anotherUserId";
+
+    // WHEN
+    let actualError;
+    try {
+      await checkUserIdMiddleware.use(userId, user);
+    } catch (e) {
+      actualError = e;
+    }
+
+    // THEN
+    expect(actualError).toBeInstanceOf(Unauthorized);
+  });
+  it("should not throw Unauthorized error if user id does match authorized user id", async () => {
+    // GIVEN
+    const checkUserIdMiddleware = PlatformTest.get<CheckUserIdMiddleware>(CheckUserIdMiddleware);
+    const userId = "userId";
+    const user = new User();
+    user._id = "userId";
+
+    // WHEN
+    let actualError;
+    try {
+      await checkUserIdMiddleware.use(userId, user);
+    } catch (e) {
+      actualError = e;
+    }
+
+    // THEN
+    expect(actualError).toBeUndefined();
+  });
+});
